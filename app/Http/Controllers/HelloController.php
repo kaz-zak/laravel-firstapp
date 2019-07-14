6a1529818de80887aa62fc3ed31784d3fae29719
gitbike <git_bike_840@yahoo.co.jp>
2019-07-14 20:41:27 +0900

    Helloコントローラにindexアクションを追加

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,5 +6,33 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    //
+  public function index() {
+    return <<<EOF
+<html>
+<head>
+<title>Hello/index</title>
+<style>
+  body {
+    font-size: 16px;
+    color: #999;
+  }
+
+  h1 {
+    font-size: 100px;
+    text-align: center:
+    color: #eee;
+  }
+
+  p {
+    text-align: center;
+  }
+</style>
+<body>
+  <h1>Index</h1>
+  <p>Helloコントローラのindexアクションのテスト</p>
+</body>
+</head>
+</html>
+EOF;
+  }
 }
