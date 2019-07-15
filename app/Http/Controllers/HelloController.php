5035258c547b2c12fc66d649e8093d8f9bc847c4
gitbike <git_bike_840@yahoo.co.jp>
2019-07-15 22:19:32 +0900

    シングルアクションコントローラに変更

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -1,65 +1,41 @@
 <?php
 namespace App\Http\Controllers;
 
-global $head, $style, $body, $end;
-$head = '<html><head>';
-$style = <<<EOF
-<style>
-  body {
-    font-size: 16px;
-    color: #999;
-  }
-
-  h1 {
-    font-size: 100px;
-    color: #eee;
-  }
-
-  p {
-    text-align: center;
-  }
-
-  ul {
-    text-align: center;
-    list-style-type: none;
-  }
-</style>
-EOF;
-$body = '</head><body>';
-$end = '</body></head>';
-
-function tag($tag, $text) {
-  return "<{$tag}>" . $text . "</{$tag}>";
-}
-
 use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-  public function index() {
-    global $head, $style, $body, $end;
+  public function __invoke() {
+    return <<<EOF
+<html>
+<head>
+<title>Hello</title>
+</head>
+<style>
+body {
+  font-size: 16px;
+  color: #999;
+}
 
-    $html = $head . tag('title', 'Hello/Index') . 
-            $style . 
-            $body .
-            tag('h1', 'Index') .
-            tag('p', 'This is index page!') .
-            '<a href="/hello/other">Go to other page.</a>' .
-            $end;
+h1 {
+  font-size: 100px;
+  color: #eee;
+}
 
-    return $html;
-  }
+p {
+  text-align: center;
+}
 
-  public function other() {
-    global $head, $style, $body, $end;
-
-    $html = $head . tag('title', 'Hello/Other') . 
-            $style .
-            $body .
-            tag('h1', 'Other') .
-            tag('p', 'This is other page.') .
-            $end;
-
-    return $html;
+ul {
+  text-align: center;
+  list-style-type: none;
+}
+</style>
+<body>
+  <h1>Single Action</h1>
+  <p>シングルコントローラのアクションを実行しています。</p>
+</body>
+</html>
+EOF;
   }
 }
