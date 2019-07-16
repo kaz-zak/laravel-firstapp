0dd7248e552b45555975f5cb3a5b952b0521d2b9
gitbike <git_bike_840@yahoo.co.jp>
2019-07-16 13:37:20 +0900

    コントローラでviewするように変更

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,43 +6,7 @@ use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-  public function index(Request $request, Response $response) {
-    $html = <<<EOF
-<html>
-<head>
-<title>Hello/Index</title>
-</head>
-<style>
-body {
-  font-size: 16px;
-  color: #999;
-}
-
-h1 {
-  font-size: 100px;
-  color: #eee;
-}
-
-p {
-  text-align: center;
-}
-
-ul {
-  text-align: center;
-  list-style-type: none;
-}
-</style>
-<body>
-  <h1>Hello</h1>
-  <h2>Request</h2>
-  <pre>{$request}</pre>
-  <h2>Response</h2>
-  <pre>{$response}</pre>
-</body>
-</html>
-EOF;
-
-  $response->setContent($html);
-  return $response;
+  public function index() {
+    return view('hello.index');
   }
 }
