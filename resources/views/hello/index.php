8601cdf27c9c8a3c78ab523be702e891b844a4a6
gitbike <git_bike_840@yahoo.co.jp>
2019-07-16 13:15:36 +0900

    テンプレートを追加

---
+++ resources/views/hello/index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+  <title>Hello/index</title>
+  <style>
+    body {
+      font-size: 16px;
+      color: #999;
+    }
+
+    h1 {
+      font-size: 100px;
+      color: #f6f6f6;
+    }
+  </style>
+</head>
+
+<body>
+  <h1>Index</h1>
+  <p>This is a sample page with template-engin Blade.</p>
+</body>
+</html>
