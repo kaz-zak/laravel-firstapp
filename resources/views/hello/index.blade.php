4720cdc38b8d989dc02428431e8408510bd20124
gitbike <git_bike_840@yahoo.co.jp>
2019-08-05 14:09:44 +0900

    SQLiteにアクセスするためDBクラスのメソッドを使う

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,21 +9,19 @@
 @endsection
 
 @section('content')
-  <p>{{ $msg }}</p>
-
-  @if (count($errors) > 0)
-  <p>入力内容に不備があります。再入力をお願いします。</p>
-  @endif
 
   <table>
-    <form action="/hello" method="post">
-      {{ csrf_field() }}
-      @if ($errors->has('msg'))
-        <tr><th>Error: </th><td>{{ $errors->first('msg') }}</td></tr>
-      @endif
-      <tr><th>Message: </th><td><input type="text" name="msg" value="{{ old('msg') }}"></td></tr>
-      <tr><th></th><td><input type="submit" value="送信"></td></tr>
-    </form>
+
+    <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+
+    @foreach ($items as $item)
+    <tr>
+      <td>{{ $item->name }}</td>
+      <td>{{ $item->mail }}</td>
+      <td>{{ $item->age }}</td>
+    </tr>
+    @endforeach
+
   </table>
 
 @endsection
