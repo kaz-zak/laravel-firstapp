486d8ed0776dc481e6cf4d9be2d4765f4cf49a68
gitbike <git_bike_840@yahoo.co.jp>
2019-08-06 19:26:39 +0900

    addアクション,createアクション追加

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -26,4 +26,22 @@ class HelloController extends Controller
     $items = DB::select('select * from people');
     return view('hello.index', ['items' => $items]);
   }
+
+  public function add(Request $request)
+  {
+    return view('hello');
+  }
+
+  public function create(Request $request)
+  {
+    $param = [
+      'name' => $request->name,
+      'mail' => $request->mail,
+      'age' => $request->age,
+    ];
+
+    DB::insert('insert into people'(name, mail, age) values (:name, :mail, :age), $param);
+
+    return redirect('/hello');
+  }
 }
