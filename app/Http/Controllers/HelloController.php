ea3e4c031ea090230202a478e4326e3435c1edeb
gitbike <git_bike_840@yahoo.co.jp>
2019-08-15 22:18:29 +0900

    deleteをクエリビルダに変更 updateがうまくいかないので戻す

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -42,9 +42,10 @@ class HelloController extends Controller
 
   public function edit(Request $request)
   {
-    $item = DB::table('people')->where('id', $request->id)->first();
+    $param = ['id' => $request->id];
+    $item = DB::select('select * from people where id = :id', $param);
 
-    return view('hello.edit', ['form' => $item]);
+    return view('hello.edit', ['form' => $item[0]]);
   }
 
   public function update(Request $request)
@@ -55,25 +56,22 @@ class HelloController extends Controller
       'mail' => $request->mail,
       'age' => $request->age,
     ];
-    
-    DB::table('people')->where('id', $request->id)->update($param);
+    DB::update('update people set name = :name mail = :mail age = :age where id = :id', $param);
     return redirect('/hello');
   }
 
   public function del(Request $request)
   {
-    $param = ['id' => $request->id];
-    $item = DB::select('select * from people where id = :id', $param);
+    $item = DB::table('people')->where('id', $request->id)->first();
 
-    return view('hello.del', ['form' => $item[0]]);
+    return view('hello.del', ['form' => $item]);
   }
 
   public function remove(Request $request)
   {
-    $param = ['id' => $request->id];
-    DB::delete('delete from people where id = :id', $param);
+    DB::table('people')->where('id', $request->id)->delete();
 
-    return redirect('hello');
+    return redirect('/hello');
   }
 
   public function show(Request $request)
