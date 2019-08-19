56ea522062fffad0921e52f55d63dae2a7ddb115
gitbike <git_bike_840@yahoo.co.jp>
2019-08-19 19:34:06 +0900

    Seeding実行

--- database/seeds/PeopleTableSeeder.php
+++ database/seeds/PeopleTableSeeder.php
@@ -12,26 +12,26 @@ class PeopleTableSeeder extends Seeder
      */
     public function run()
     {
-      $param = [
-        'name' => 'taro',
-        'mail' => 'taro@yamamoto.co.jp',
-        'age' => 40,
-      ];
-      DB:table('people')->insert($param);
+        $param = [
+            'name' => 'taro',
+            'mail' => 'taro@yamamoto.co.jp',
+            'age' => 40,
+        ];
+        DB::table('people')->insert($param);
 
-      $param = [
-        'name' => 'hanako',
-        'mail' => 'hana@flower.com',
-        'age' => 12,
-      ];
-      DB::table('people')->insert($param);
+        $param = [
+            'name' => 'hanako',
+            'mail' => 'hana@flower.com',
+            'age' => 12,
+        ];
+        DB::table('people')->insert($param);
 
-      $param = [
-        'name' => 'sachiko',
-        'mail' => 'sachi@happy.go,jp',
-        'age' => 60,
-      ];
-      DB::table('people')->insert($param);
+        $param = [
+            'name' => 'sachiko',
+            'mail' => 'sachi@happy.go,jp',
+            'age' => 60,
+        ];
+        DB::table('people')->insert($param);
 
     }
 }
