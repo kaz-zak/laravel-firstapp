e99e443e117c7e2823f57b75d2a8188e4e39f6f6
gitbike <git_bike_840@yahoo.co.jp>
2019-09-02 19:53:39 +0900

    restのcreateを利用するテンプレートを作成

---
+++ resources/views/hello/rest.blade.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+    <title>hello/Rest</title>
+    <style>
+        body {
+            margin: 5px;
+            font-size; 16px;
+            color: $999;
+        }
+
+        h1 {
+            margin: -20px 0 -30px 0;
+            font-size: 50px;
+            text-align: right;
+            color: #f6f6f6;
+            letter-spacing: -4px;
+        }
+
+        th {
+            padding: 5px 10px;
+            background-color: #999;
+            color: #fff;
+        }
+
+        td {
+            padding: 5px 10px;
+            border: 1px solid #aaa;
+            color: #999;
+        }
+
+        .content {
+            margin: 10px;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Rest</h1>
+    @include('rest.create')
+</body>
+</html>
