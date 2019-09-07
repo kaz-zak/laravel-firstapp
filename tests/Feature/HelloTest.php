b3c502186b6e94bb80b256da7b7c55769b9980b7
gitbike <git_bike_840@yahoo.co.jp>
2019-09-07 21:37:55 +0900

    テストを追加

--- tests/Feature/HelloTest.php
+++ tests/Feature/HelloTest.php
@@ -13,10 +13,24 @@ class HelloTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    // public function testExample()
+    // {
+    //     $response = $this->get('/');
 
-        $response->assertStatus(200);
+    //     $response->assertStatus(200);
+    // }
+
+    public function testHello()
+    {
+        $this->assertTrue(true);
+
+        $arr = [];
+        $this->assertEmpty($arr);
+
+        $msg = 'Hello!';
+        $this->assertEquals('Hello!', $msg);
+
+        $n = random_int(0, 100);
+        $this->assertLessThan(100, $n);
     }
 }
